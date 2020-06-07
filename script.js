e4b30e08f7f7d9ae49240b14d51cf2ac9da56cb9
Charles Ivia <iviacharles@gmail.com>
2020-06-07 18:13:27 +0300

    Fetched dom elements

---
+++ script.js
@@ -0,0 +1,6 @@
+//Fetch dom elements
+const main = document.getElementById("main");
+const addUser = document.getElementById('add-user')
+const doubleBtn = document.getElementById('double')
+const showMillionairesBtn = document.getElementById('show-millionaires')
+const sortBtn = document.getElementById('sort')
